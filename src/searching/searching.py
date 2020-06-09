676403c5fd171db46862bede7b686d01414429da
CScori <49799016+CScori@users.noreply.github.com>
2020-06-09 12:55:48 -0700

    sort of makes sense

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,16 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
-
-
+    for i, ele in enumerate(arr):
+        if ele == target: 
+            return i 
+    
     return -1  # not found
